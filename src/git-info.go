d5bb082361552c0f39249303d25aebf328898e5f
indrajit <eendroroy@gmail.com>
2017-08-26 12:11:41 +0600

    dummy

--- src/git-info.go
+++ src/git-info.go
@@ -25,4 +25,32 @@ func main() {
   }
 
   fmt.Println(default_remote.Name())
+
+  bi,_ := repo.NewBranchIterator(git.BranchRemote)
+
+  bi.ForEach(func(branch *git.Branch, branchType git.BranchType) error {
+    br_name, _ := branch.Name()
+    fmt.Println(br_name)
+    return nil
+  })
+
+  odb, _ := repo.Odb()
+
+  count := 0
+
+  odb.ForEach(func(id *git.Oid) error {
+    commit, err := repo.LookupCommit(id)
+    if err == nil {
+      fmt.Println(
+        helpers.Colorize(commit.Committer().Name, helpers.Blue()),
+        helpers.Colorize(commit.Committer().Email, helpers.Green()),
+        helpers.Colorize(commit.Message(), helpers.White()),
+        commit.ParentCount(),
+      )
+    }
+    count++
+    return nil
+  })
+
+  fmt.Println(count)
 }
